3c64107ed5543153aca7411b18484c8550bd1b67
SIMONE CANDIDO s337165 <s337165@studenti.polito.it>
2025-04-02 09:10:51 +0200

    feat: enhance BagsList component with responsive layout and improved card display

--- rescueSurplusFood-app/src/components/bag/BagsList.jsx
+++ rescueSurplusFood-app/src/components/bag/BagsList.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 import BagCard from './BagCard';
+import '../../styles/style.css'; // Importa un file CSS
 
-function BagsList({ bags, addToCart }) {
+function BagsList({ bags }) {
   return (
-    <Row>
+    <Row className="row-cols-1 row-cols-md-3 g-4 same-height-cards pb-4"> {/* Aggiungi la classe custom */}
       {bags.map(bag => (
-        <Col key={bag.id} md={4} className="mb-4">
-          <BagCard bag={bag} addToCart={addToCart} />
+        <Col key={bag.id}>
+          <BagCard bag={bag} />
         </Col>
       ))}
     </Row>
